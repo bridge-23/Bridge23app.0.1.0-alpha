ced3572513f14fbd8bed801bf92e7d10a20771e1
bridge-23 <nezovskii@bridge23.tech>
2023-07-25 13:18:26 +0800

    Claim example without 5 for one only matic

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from "next/app";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
-import Navbar from "../Components/Navbar";
+import Navbar from "../Components/navbar";
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
@@ -10,7 +10,7 @@ const activeChain = "mumbai";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-      <ThirdwebProvider activeChain={activeChain}>
+      <ThirdwebProvider activeChain={activeChain} clientId="43fb0cedb2a9e9359d2103814a1be01b">
         <Navbar />
         <Component {...pageProps} />
       </ThirdwebProvider>
