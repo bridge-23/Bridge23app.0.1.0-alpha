f280fefa8399ec81adf8fb31aff241e4ea20ca4b
bridge-23 <nezovskii@bridge23.tech>
2023-07-28 12:06:06 +0800

    Add example components, app page is same

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
 import Navbar from "../Components/navbar";
+import { useEffect } from 'react';
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
@@ -9,11 +10,20 @@ import Navbar from "../Components/navbar";
 const activeChain = "mumbai";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const clientId = process.env.CLIENT_ID;
+    const secretKey = process.env.SECRET_KEY;
+
+    // Use the clientId and secretKey as needed in your application logic.
+    // For example, you can pass them as props to other components or use them in API calls.
+
+  }, []);
+
   return (
-      <ThirdwebProvider activeChain={activeChain} clientId="43fb0cedb2a9e9359d2103814a1be01b">
-        <Navbar />
-        <Component {...pageProps} />
-      </ThirdwebProvider>
+    <ThirdwebProvider activeChain={activeChain} clientId="0f7b5a725f342d3d2ffe12e6303b0f4b">
+      <Navbar />
+      <Component {...pageProps} />
+    </ThirdwebProvider>
   );
 }
 
