e15e4732d6cfb229bf7a859df1eb612fb26fe34f
bridge-23 <nezovskii@bridge23.tech>
2023-08-08 18:47:38 +0800

    Update Burn contract address

--- consts/parameters.ts
+++ consts/parameters.ts
@@ -1,4 +1,4 @@
 export const CONTRACT_ADDRESS = '0x27A9afa27c7b39744ad51B2031a439e7DF7BD149';
-export const BURN_ADDRESS = '0x62d1d90a655abB3483307D0E5f3A239D926e1706';
+export const BURN_ADDRESS = '0xf2F1bABF56F5e55F473B81Bec33E60C66DAEf5cc';
 export const chain = "mumbai";
 export const blockExplorer = "https://mumbai.polygonscan.com/";
