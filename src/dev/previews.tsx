b2989f9b3df852aea2b1ede989360188cedf29bf
bridge-23 <nezovskii@bridge23.tech>
2023-08-30 23:08:32 +0800

    const handleNftChange изменить на число, для отображения в select

--- src/dev/previews.tsx
+++ src/dev/previews.tsx
@@ -4,10 +4,14 @@ import {PaletteTree} from "./palette";
 import Claim from "../pages/claim/[walletAddress]";
 
 const ComponentPreviews = () => {
+    const contractMetadata = {
+        // ... your contract metadata here
+    };
+
     return (
         <Previews palette={<PaletteTree/>}>
             <ComponentPreview path="/Claim">
-                <Claim/>
+                <Claim contractMetadata={contractMetadata}/>
             </ComponentPreview>
         </Previews>
     );
