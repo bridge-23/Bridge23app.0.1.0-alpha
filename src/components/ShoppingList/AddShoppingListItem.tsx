900e2c0fb10d44874e9243a77bd88669691f4f1e
laska18934593845834785 <Nezovskii.info@gmail.com>
2023-11-10 10:45:59 +0800

    Fix 4 issue

--- src/components/ShoppingList/AddShoppingListItem.tsx
+++ src/components/ShoppingList/AddShoppingListItem.tsx
@@ -1,13 +1,13 @@
 //..src/components/ShoppingList/AddShoppingListItem.tsx
-import React, { useState, useEffect, FC, useContext } from "react";
-import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
-import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import DialogContent from "@mui/material/DialogContent";
-import DialogTitle from "@mui/material/DialogTitle";
-import IconButton from "@mui/material/IconButton";
-import CloseIcon from "@mui/icons-material/Close";
+import React, {useState, FC, useContext} from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
 import { setDoc } from "@junobuild/core-peer";
 //import { initializeJuno } from '../../lib/Juno/initJuno';
 import { nanoid } from "nanoid";
@@ -18,43 +18,26 @@ interface AddShoppingListNoteProps {
   userId: string; // Assuming you pass the user ID as a prop
 }
 
-const AddShoppingListItem: FC<AddShoppingListNoteProps> = ({
-  open,
-  onClose,
-}) => {
-  const [note, setNote] = useState<string>("");
-  const [junoReady, setJunoReady] = useState<boolean>(false);
-  const { user } = useContext(AuthContext);
-
-  /*    useEffect(() => {
-        async function init() {
-            setJunoReady(true);
-        }
-        init();
-    }, []);*/
-
-  const handleAddNote = async () => {
-    if (!note.trim()) {
-      alert("Please provide a valid note.");
-      return;
-    }
-
-    /*        if (!junoReady) {
-            alert('Application is initializing, please try again in a moment.');
+const AddShoppingListItem: FC<AddShoppingListNoteProps> = ({ open, onClose }) => {
+    const [note, setNote] = useState<string>('');
+    const { user } = useContext(AuthContext);
+    const handleAddNote = async () => {
+        if (!note.trim()) {
+            alert('Please provide a valid note.');
             return;
-        }*/
+        }
 
-    try {
-      const noteId = `${nanoid()}`;
-      await setDoc({
-        collection: "ShoppingList",
-        doc: {
-          key: noteId,
-          data: {
-            content: note,
-          },
-        },
-      });
+        try {
+            const noteId = `${nanoid()}`;
+            await setDoc({
+                collection: "ShoppingList",
+                doc: {
+                    key: noteId,
+                    data: {
+                        content: note
+                    }
+                }
+            });
 
       alert("Note added successfully!");
       setNote("");
@@ -91,22 +74,19 @@ const AddShoppingListItem: FC<AddShoppingListNoteProps> = ({
           rows={4}
         />
       </DialogContent>
-
-      <DialogActions>
-        <Button onClick={onClose} color="secondary">
-          Cancel
-        </Button>
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={handleAddNote}
-          disabled={!junoReady}
-        >
-          Add Note
-        </Button>
-      </DialogActions>
-    </Dialog>
-  );
-};
-
+            <DialogActions>
+                <Button onClick={onClose} color="secondary">
+                    Cancel
+                </Button>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={handleAddNote}
+                >
+                    Add Note
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
+}
 export default AddShoppingListItem;
